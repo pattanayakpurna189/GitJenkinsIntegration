c03cb0654b1158ce9de8e55538175e2c9b5a351d
pattanayakpurna189 <36988096+pattanayakpurna189@users.noreply.github.com>
2021-04-29 01:16:29 +0530

    Update Testing.java
    jen git 4

--- src/main/java/Execution/Testing.java
+++ src/main/java/Execution/Testing.java
@@ -31,7 +31,7 @@ public class Testing {
 		System.out.println(links.get(2).toString());
 		driver.quit();
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
