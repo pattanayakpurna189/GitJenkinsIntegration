bb0196cb4129678b7a90c51c970fc4f8b86fda7c
admin <pattanayak.purna@outlook.com>
2021-04-28 22:49:55 +0530

    updated pom file with maven compiler and surefile plugins

--- src/main/java/Execution/Testing.java
+++ src/main/java/Execution/Testing.java
@@ -28,43 +28,6 @@ public class Testing {
 		driver.get("https://chromedriver.chromium.org/downloads");
 		driver.manage().window().maximize();
 		List<WebElement> links = driver.findElements(By.tagName("a"));
-
-		Iterator<WebElement> it = links.iterator();
-
-		while(it.hasNext()){
-
-				url = it.next().getAttribute("href");
-		
-				//System.out.println(url);
-		
-				if(url == null || url.isEmpty()){
-					System.out.println("URL is either not configured for anchor tag or it is empty");
-					continue;
-				}
-			
-				try {
-					huc = (HttpURLConnection)(new URL(url).openConnection());
-			
-					huc.setRequestMethod("HEAD");
-			
-					huc.connect();
-			
-					respCode = huc.getResponseCode();
-			
-					if(respCode >= 400){
-					  System.out.println(url+" is a broken link");
-					}
-					else{
-					  System.out.println(url+" is a valid link");
-					}
-		
-				} catch (MalformedURLException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		}
-
 		driver.quit();
 		
 		
